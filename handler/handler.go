3161da3bc90f87305a4b778b18211e5d92dd8e8d
buxiaomo <95112082@qq.com>
2025-03-05 12:10:11 +0800

    fix: 修复Docker认证相关问题

--- handler/handler.go
+++ handler/handler.go
@@ -126,6 +126,7 @@ func DockerHandler(c *gin.Context) {
 	if resp.StatusCode == http.StatusUnauthorized {
 		authHeader := resp.Header.Get("WWW-Authenticate")
 		log.Printf("[DockerHandler] 收到认证头: %s", authHeader)
+		fmt.Println(authHeader != "")
 		if authHeader != "" {
 			// 解析认证信息
 			realm, service, scope := parseAuthHeader(authHeader)
